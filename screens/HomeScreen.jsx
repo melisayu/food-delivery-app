1f0ad66a516ed4e85cb597fc5e252f8ed12b1f6e
melisayu <melisayuliakristin@gmail.com>
2021-03-14 18:57:46 +0100

    Add Search Input at HomeScreen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, TextInput, ScrollView, StyleSheet } from 'react-native';
 
 import ItemCard from '../components/ItemCard';
 
@@ -17,7 +17,8 @@ function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.homeTitle}>Delicious foods for you</Text>
-      <ScrollView horizontal>
+      <TextInput placeholder="Search" style={styles.textInput} />
+      <ScrollView horizontal style={styles.itemsContainer}>
         {foods.length > 0 && foods.map((food) => (
           <ItemCard
             item={food}
@@ -33,10 +34,24 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 50,
   },
   homeTitle: {
     fontSize: 34,
     fontWeight: '700',
+    marginBottom: 28,
+  },
+  textInput: {
+    backgroundColor: '#EFEEEE',
+    borderRadius: 30,
+    fontSize: 17,
+    width: '100%',
+    height: 60,
+    padding: 16,
+    marginBottom: 46,
+  },
+  itemsContainer: {
+    width: '110%',
   }
 })
 
