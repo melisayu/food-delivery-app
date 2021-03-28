c78392c149af220f8ce78565ba6f823ca28dff16
melisayu <melisayuliakristin@gmail.com>
2021-03-28 21:25:26 +0200

    Add BottomTabs Component

--- components/BottomTabs.jsx
+++ components/BottomTabs.jsx
@@ -1,14 +1,115 @@
 import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
-function BottomTabs() {  
+function BottomTabs({ state, descriptors, navigation }) {
+  const focusedOptions = descriptors[state.routes[state.index].key].options;
+
+  if (focusedOptions.tabBarVisible === false) {
+    return null;
+  }
+
+  const getIcon = routeName => {
+    switch(routeName) {
+      case 'Home':
+        return (
+          <Icon.Button
+            name="home"
+            backgroundColor="#fff"
+            color="#FA4A0C"
+            style={styles.icon}
+          >
+            <Text>{routeName}</Text>
+          </Icon.Button>
+        );
+      case 'Favorite':
+        return (
+          <Icon.Button
+            name="heart"
+            backgroundColor="#fff"
+            color="#FA4A0C"
+            onPress={console.log('go to favorite page')}
+            style={styles.icon}
+          >
+            <Text>{routeName}</Text>
+          </Icon.Button>
+        )
+      case 'Profile':
+        return (
+          <Icon.Button
+            name="user"
+            backgroundColor="#fff"
+            color="#FA4A0C"
+            style={styles.icon}
+          >
+            <Text>{routeName}</Text>
+          </Icon.Button>
+        )
+      case 'PastOrders':
+        return (
+          <Icon.Button
+            name="back-in-time"
+            backgroundColor="#fff"
+            color="#FA4A0C"
+            style={styles.icon}
+          >
+            <Text>{routeName}</Text>
+          </Icon.Button>
+        )
+      default:
+        return null;
+    }
+  }
+
   return (
-    <View>
-      <TouchableOpacity>
-        <Text>Home</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      {state.routes.map((route, index) =>   {
+        const { options } = descriptors[route.key];
+        const label = options.tabBarLabel !== undefined
+                      ? options.tabBarLabel
+                      : options.title !== undefined
+                      ? options.title
+                      : route.name;
+        const isFocused = state.index === index;
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        return (
+          <TouchableOpacity
+            key={label}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            style={styles.tabContainer}
+          >
+            {getIcon(label)}
+          </TouchableOpacity>
+        )
+      })}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    backgroundColor: 'white',
+  },
+  icon: {
+    flexDirection: 'column'
+  }
+})
 
 export default BottomTabs;
--- components/BottomTabsNavigator.jsx
+++ components/BottomTabsNavigator.jsx
@@ -1,32 +1,67 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BlurView, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 import HomeScreen from '../screens/HomeScreen';
 import FavoriteScreen from '../screens/FavoriteScreen';
+import BottomTabs from './BottomTabs';
 
 const HomeScreenStack = createStackNavigator();
 const FavoriteStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+const PastOrdersStack = createStackNavigator();
 
 const Home = () => (
   <HomeScreenStack.Navigator>
-    <HomeScreenStack.Screen name="HomeScreen" component={HomeScreen}></HomeScreenStack.Screen>
+    <HomeScreenStack.Screen
+      name="HomeScreen"
+      component={HomeScreen}
+      options={{
+        headerShown: false,
+      }}
+    />
   </HomeScreenStack.Navigator>
 )
 
 const Favorite = () => (
   <FavoriteStack.Navigator>
-    <FavoriteStack.Screen name="FavoriteScreen" component={FavoriteScreen}></FavoriteStack.Screen>
+    <FavoriteStack.Screen
+      name="FavoriteScreen"
+      component={FavoriteScreen}
+      options={{
+        headerLeft: () => <Icon.Button name="heart"></Icon.Button>
+      }}
+    />
   </FavoriteStack.Navigator>
+)
+
+const Profile = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen
+      name="ProfileScreen"
+    />
+  </ProfileStack.Navigator>
+)
+
+const PastOrders = () => (
+  <PastOrdersStack.Navigator>
+    <PastOrdersStack.Screen
+      name="PastOrdersScreen"
+    />
+  </PastOrdersStack.Navigator>
 )
 
 const BottomTab = createBottomTabNavigator();
 
 function BottomTabsNavigator() {
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator tabBar={props => <BottomTabs {...props} />}>
       <BottomTab.Screen name="Home" component={Home} />
       <BottomTab.Screen name="Favorite" component={Favorite} />
+      <BottomTab.Screen name="Profile" component={Profile} />
+      <BottomTab.Screen name="PastOrders" component={PastOrders} />
     </BottomTab.Navigator>
   )
 }
